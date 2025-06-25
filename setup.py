f8d1cab2356775cc352b555f23291bee7d77bbc9
cyberiuscompany <cyberiuscompany@gmail.com>
2025-06-25 21:29:47 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cyusbguard",
+    version="1.0.0",
+    description="Herramienta de análisis de USBs con integración en VirusTotal",
+    author="Cyberius Company",
+    author_email="contacto@cyberiuscompany.com",
+    url="https://github.com/cyberiuscompany/CyUSBGuard",
+    packages=find_packages(),
+    py_modules=["cyusbguard", "ver_informe_en_frame"],
+    include_package_data=True,
+    install_requires=[
+        "pystray",
+        "Pillow",
+        "requests"
+    ],
+    entry_points={
+        "console_scripts": [
+            "cyusbguard=cyusbguard:lanzar_app"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.7',
+)
